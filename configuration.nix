22aad26bff3331898ee6a651454d6ed044ae7036
Ezekiel Bethel <zek@9net.org>
2021-12-28 20:37:44 +0000

    e

--- configuration.nix
+++ configuration.nix
@@ -50,48 +50,12 @@ let unstable = import <nixos-unstable> {
 
   environment.systemPackages = with pkgs; [
      wget
-     google-chrome
-     autorandr
      git
      yadm
-     rofi
-     dex
-     picom
-     feh
-     xbindkeys
-     pavucontrol
-     rxvt-unicode
-     polybar
      killall
-
-     slack
-     discord
-     hexchat
-     todoist-electron
-     signal-desktop
-    
-     python3
-     rustup
-
      file
-     carla
-     jack2
-     libvirt
-     virt-manager
-
-     sublime4
-     maim
-     slop
-     xclip
-     libreoffice
-
-     # https://github.com/NixOS/nixpkgs/issues/2448
-     dconf
-     kicad-unstable
-
-     rustup
-     ddcutil
      cron
+     rxvt_unicode.terminfo
   ];
 
   # Enable the OpenSSH daemon.
